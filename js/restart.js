3836e8df940c3b44874ae76b44346bf0fe0d8c75
kenneth.vangijsel <kenneth.vangijsel@gmail.com>
2019-09-06 11:36:11 +0200

    fail --> start christos anyway

--- js/restart.js
+++ js/restart.js
@@ -1,14 +1,17 @@
-const RESTART_URL = "http://christos:8090/restart/";
-// const RESTART_URL = "http://localhost:8090/restart/";
+const RESTART_URL = "http://christos:8090/";
+// const RESTART_URL = "http://localhost:8090/";
 const restart = $('#restart');
 
 $(restart).click(() => {
-    $.getJSON(RESTART_URL,
+    $.getJSON(RESTART_URL + "restart",
         (response) => {
             console.log(response);
-            window.open("http://christos:8090", "_blank");
         }
     ).done(() => {
+        window.open(RESTART_URL, "_blank");
+        alert("Backend succesvol herstart!");
+    }).fail(() => {
+        window.open(RESTART_URL, "_blank");
         alert("Backend succesvol herstart!");
     });
 });
