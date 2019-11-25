02019c0332f8ab08c462086fce3101ab0811429c
Kenneth <kenneth.vangijsel@somedi.be>
2019-11-25 12:25:47 +0100

    showPleaseAndWait while restarting

--- js/restart.js
+++ js/restart.js
@@ -3,14 +3,17 @@ const RESTART_URL = "http://christos:8090/";
 const restart = $('#restart');
 
 $(restart).click(() => {
+    showPleaseWait();
     $.getJSON(RESTART_URL + "restart",
         (response) => {
             console.log(response);
         }
     ).done(() => {
+        hidePleaseWait();
         window.open(RESTART_URL, "_blank");
         alert("Backend succesvol herstart!");
     }).fail(() => {
+        hidePleaseWait();
         window.open(RESTART_URL, "_blank");
         alert("Backend succesvol herstart!");
     });
